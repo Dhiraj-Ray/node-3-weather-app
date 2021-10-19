cea7d18c2b91cce2f04ee91424debdcb3c241a7b
Dhiraj Ray <dhiraj.ray1995@gmail.com>
2021-10-19 15:29:04 +0530

    =variable added those have been missed

--- public/js/app.js
+++ public/js/app.js
@@ -7,7 +7,7 @@ fetch("http://puzzle.mead.io/puzzle").then ((response)=>{
 const weatherform=document.querySelector('form');
 const search=document.querySelector("input");
 const messageOne=document.querySelector("#message-1")
-messageTwo=document.querySelector("#message-2")
+ const messageTwo=document.querySelector("#message-2")
 // messageOne.textContent="From javascript"
 weatherform.addEventListener("submit",(e)=>{
     e.preventDefault()
